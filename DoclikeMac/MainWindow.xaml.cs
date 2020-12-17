ab457401bd0e1cb79af3fdfb70ca8c3a0a62cc79
mita <cpjvmm@outlook.jp>
2020-12-18 00:05:23 +0900

    透明ウィンドウ実装

--- DoclikeMac/MainWindow.xaml.cs
+++ DoclikeMac/MainWindow.xaml.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Threading;
 
 namespace DoclikeMac
 {
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+
+    internal enum WindowCompositionAttribute
+    {
+        WCA_ACCENT_POLICY = 19
+    }
+
+    internal enum AccentState
+    {
+        ACCENT_DISABLED = 0,
+        ACCENT_ENABLE_GRADIENT = 1,
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+        ACCENT_ENABLE_BLURBEHIND = 3,
+        ACCENT_INVALID_STATE = 4
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public uint GradientColor;
+        public int AnimationId;
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -12,10 +45,13 @@ namespace DoclikeMac
     {
         //画面解像度(横)
         private double screenWidth;
+
         //画面解像度(縦)
         private double screenHeight;
+
         //調整高さ幅
         private const int pad = 1;
+
         //ウィンドウy座標最小値
         private double minY;
 
@@ -23,7 +59,8 @@ namespace DoclikeMac
         private DispatcherTimer timer = null;
 
         //ウィンドウ表示・非表示スピード
-        private readonly int spf = 33;
+        private readonly int spf = 16;
+
         private float delta = -4f;
 
         /// <summary>
@@ -38,7 +75,43 @@ namespace DoclikeMac
             Left = (screenWidth - Width) / 2;
             Top = screenHeight;
             minY = screenHeight - Height + pad;
-            Opacity = 0;
+        }
+
+        /// <summary>
+        /// 引用コード
+        /// ぼかし効果の適用
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            EnableBlur(this);
+        }
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        internal static void EnableBlur(Window win)
+        {
+            var windowHelper = new WindowInteropHelper(win);
+
+            var accent = new AccentPolicy();
+            var accentStructSize = Marshal.SizeOf(accent);
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+            accent.AccentFlags = 2;
+            accent.GradientColor = 0x66FFFFFF;
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData
+            {
+                Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+                SizeOfData = accentStructSize,
+                Data = accentPtr
+            };
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+            Marshal.FreeHGlobal(accentPtr);
         }
 
         /// <summary>
@@ -64,7 +137,6 @@ namespace DoclikeMac
                 else if (Top > screenHeight && delta < 0)
                 {
                     Top = screenHeight;
-                    Opacity = 0;
                     t.Stop();
                 }
             };
@@ -87,32 +159,25 @@ namespace DoclikeMac
         }
 
         /// <summary>
-        /// 画面下にカーソルを持ってきたときに起動するメソッド
+        /// ウィンドウを動かす処理
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        private void AnimationWindow()
         {
             delta *= -1;
-            Opacity = 100;
             if (timer != null) timer.Stop();
             timer = CreateTimer();
             timer.Start();
             GC.Collect();
         }
 
-        /// <summary>
-        /// ウィンドウからカーソルが離れたときに起動するメソッド
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationWindow();
+        }
+
         private void Window_MouseLeave(object sender, MouseEventArgs e)
         {
-            delta *= -1;
-            if (timer != null) timer.Stop();
-            timer = CreateTimer();
-            timer.Start();
-            GC.Collect();
+            AnimationWindow();
         }
     }
 }
