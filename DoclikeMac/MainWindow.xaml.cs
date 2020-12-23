5e6bd13dce0b069dc0138da106a690026d0a580a
mita <cpjvmm@outlook.jp>
2020-12-23 18:39:13 +0900

    起動時: 微修正

--- DoclikeMac/MainWindow.xaml.cs
+++ DoclikeMac/MainWindow.xaml.cs
@@ -1,43 +1,10 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Threading;
 
 namespace DoclikeMac
 {
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct WindowCompositionAttributeData
-    {
-        public WindowCompositionAttribute Attribute;
-        public IntPtr Data;
-        public int SizeOfData;
-    }
-
-    internal enum WindowCompositionAttribute
-    {
-        WCA_ACCENT_POLICY = 19
-    }
-
-    internal enum AccentState
-    {
-        ACCENT_DISABLED = 0,
-        ACCENT_ENABLE_GRADIENT = 1,
-        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
-        ACCENT_ENABLE_BLURBEHIND = 3,
-        ACCENT_INVALID_STATE = 4
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct AccentPolicy
-    {
-        public AccentState AccentState;
-        public int AccentFlags;
-        public uint GradientColor;
-        public int AnimationId;
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -61,7 +28,7 @@ namespace DoclikeMac
         //ウィンドウ表示・非表示スピード
         private readonly int spf = 16;
 
-        private float delta = -4f;
+        private float delta = 4f;
 
         /// <summary>
         /// コンストラクタ
@@ -73,45 +40,9 @@ namespace DoclikeMac
             screenWidth = SystemParameters.PrimaryScreenWidth;
             screenHeight = SystemParameters.PrimaryScreenHeight - pad;
             Left = (screenWidth - Width) / 2;
-            Top = screenHeight;
+            Top = screenHeight - Height;
             minY = screenHeight - Height + pad;
-        }
-
-        /// <summary>
-        /// 引用コード
-        /// ぼかし効果の適用
-        /// </summary>
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            EnableBlur(this);
-        }
-
-        [DllImport("user32.dll")]
-        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
-
-        internal static void EnableBlur(Window win)
-        {
-            var windowHelper = new WindowInteropHelper(win);
-
-            var accent = new AccentPolicy();
-            var accentStructSize = Marshal.SizeOf(accent);
-            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
-            accent.AccentFlags = 2;
-            accent.GradientColor = 0x66FFFFFF;
-
-            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
-            Marshal.StructureToPtr(accent, accentPtr, false);
-
-            var data = new WindowCompositionAttributeData
-            {
-                Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
-                SizeOfData = accentStructSize,
-                Data = accentPtr
-            };
-
-            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
-            Marshal.FreeHGlobal(accentPtr);
+            AnimationWindow();
         }
 
         /// <summary>
