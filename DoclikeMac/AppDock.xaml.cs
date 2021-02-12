a7e671549f6a6fcadcd0bef22f2e52405eae196d
mita <cpjvmm@outlook.jp>
2021-02-12 11:25:59 +0900

    folderdock表示位置再修正

--- DoclikeMac/AppDock.xaml.cs
+++ DoclikeMac/AppDock.xaml.cs
@@ -262,7 +262,7 @@ namespace DocklikeMac
         {
             fdWindow = new FolderDock();
             fdWindow.Top = (screenHeight - fdWindow.Height) / 2;
-            fdWindow.Left = 0;
+            fdWindow.Left = screenWidth - fdWindow.Width;
             fdWindow.Show();
         }
 
